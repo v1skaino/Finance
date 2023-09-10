37eb8b6a6c9add4d20d51f6d15e5547d91984181
dev_vasco <lucas.viscaino@epiousion.com.br>
2023-09-10 14:45:00 -0300

    feat: ✨ Drawer

--- src/shared/routes/app.routes.tsx
+++ src/shared/routes/app.routes.tsx
@@ -5,8 +5,23 @@ const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
   return (
-    <AppDrawer.Navigator>
-      <AppDrawer.Screen name="Home" component={HomeView} />
+    <AppDrawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerTintColor: "#121212",
+        headerStyle: { backgroundColor: "#f0f4ff" },
+        drawerStyle: { backgroundColor: "#fff", paddingTop: 20 },
+        drawerActiveBackgroundColor: "#3b3dbd",
+        drawerActiveTintColor: "#fff",
+        drawerInactiveBackgroundColor: "#f0f0f0",
+        drawerInactiveTintColor: "#121212",
+      }}
+    >
+      <AppDrawer.Screen
+        name="Home"
+        component={HomeView}
+        options={{ headerTitle: "Minhas movimentações" }}
+      />
     </AppDrawer.Navigator>
   );
 }
