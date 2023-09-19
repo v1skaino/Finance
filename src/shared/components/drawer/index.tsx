3178e5d9f9007f7ab8a7bc0aa4aa6aa99aa30ae0
dev_vasco <lucas.viscaino@epiousion.com.br>
2023-09-19 15:34:50 -0300

    feat: ✨ Drawer

--- src/shared/components/drawer/index.tsx
+++ src/shared/components/drawer/index.tsx
@@ -4,15 +4,27 @@ import {
   DrawerItemList,
 } from "@react-navigation/drawer";
 import { DrawerContentComponentProps } from "@react-navigation/drawer/lib/typescript/src/types";
+import { Image } from "react-native";
+import styled from "styled-components/native";
 import { useGlobal } from "../../contexts/global/global.context";
 
 export const Drawer = (props: DrawerContentComponentProps) => {
-  const { methods } = useGlobal();
+  const { methods, state } = useGlobal();
 
   return (
     <DrawerContentScrollView {...props}>
+      <Wrapper>
+        <Image source={require("../../../../assets/Logo.png")} />
+      </Wrapper>
       <DrawerItemList {...props} />
       <DrawerItem label="Sair" onPress={methods?.signOut} />
     </DrawerContentScrollView>
   );
 };
+
+const Wrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0px;
+  gap: 5px;
+`;
